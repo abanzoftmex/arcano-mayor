d2ae4725c905b6be4b59ff4948627acb0b518818
Alejandro Ponce <alejandro.poncet@icloud.com>
2026-02-27 10:29:28 -0600

    feat: add Hero234 component with animated gallery and integrate into HomeBlocks

--- frontend-app/src/components/blocks/home-blocks.jsx
+++ frontend-app/src/components/blocks/home-blocks.jsx
@@ -1,11 +1,13 @@
 import { Navbar11 } from "../navbar11";
 import { Hero55 } from "../hero55";
+import { Hero234 } from "../hero234";
 
 export function HomeBlocks() {
   return (
     <>
       <Navbar11 />
       <Hero55 />
+      <Hero234 />
     </>
   );
 }
