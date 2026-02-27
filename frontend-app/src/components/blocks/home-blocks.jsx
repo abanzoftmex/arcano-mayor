ddfb2309d58245bfe6c9f3a5d45d9ed76604aa11
Alejandro Ponce <alejandro.poncet@icloud.com>
2026-02-27 12:11:46 -0600

    feat: add Feature247 component with text reveal animation and integrate into HomeBlocks

--- frontend-app/src/components/blocks/home-blocks.jsx
+++ frontend-app/src/components/blocks/home-blocks.jsx
@@ -1,6 +1,7 @@
 import { Navbar11 } from "../navbar11";
 import { Hero55 } from "../hero55";
 import { Hero234 } from "../hero234";
+import { Feature247 } from "../feature247";
 import { PromoBanner2 } from "../promo-banner2";
 
 export function HomeBlocks() {
@@ -16,6 +17,7 @@ export function HomeBlocks() {
       <Navbar11 className="top-10" />
       <Hero55 />
       <Hero234 />
+      <Feature247 />
     </>
   );
 }
