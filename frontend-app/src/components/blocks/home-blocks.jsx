542ad1788762248e30c44ced87eec47130b62a8c
Alejandro Ponce <alejandro.poncet@icloud.com>
2026-02-27 12:08:33 -0600

    feat: add PromoBanner2 component and integrate it into HomeBlocks

--- frontend-app/src/components/blocks/home-blocks.jsx
+++ frontend-app/src/components/blocks/home-blocks.jsx
@@ -1,11 +1,19 @@
 import { Navbar11 } from "../navbar11";
 import { Hero55 } from "../hero55";
 import { Hero234 } from "../hero234";
+import { PromoBanner2 } from "../promo-banner2";
 
 export function HomeBlocks() {
   return (
     <>
-      <Navbar11 />
+      <PromoBanner2
+        className="fixed top-0 z-[1000]"
+        message="Lecturas y tienda espiritual"
+        discount="10% en tu primera compra"
+        link="#tienda"
+        linkText="Ver ofertas"
+      />
+      <Navbar11 className="top-10" />
       <Hero55 />
       <Hero234 />
     </>
